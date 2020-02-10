7b43aa8e872f97475126c3d2fbfc7063c8d3b14e
Tran Phuoc Hung <admin@tygon.com.vn>
2020-02-10 12:07:34 +0700

    format

--- list-customer.php
+++ list-customer.php
@@ -1,113 +1,131 @@
 <?php
-  $titles = 'Thống kê';
+  $titles = 'Khách hàng';
   include('/widget/header.php');
 ?>
 
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <?php
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <?php
       include('widget/menu.php');
     ?>
-    <!-- SEARCH FORM -->
-    <?php
+            <?php
       include('widget/search-form.php');
     ?>
-  </nav>
-  <!-- /.navbar -->
-  <!-- Main Sidebar Container -->
-  <?php include('widget/slider-bar.php') ?>
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Danh sách khách hàng</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Thống kê</a></li>
-              <li class="breadcrumb-item active">Danh sách khách hàng</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+        </nav>
+        <!-- /.navbar -->
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-        <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Danh sách khách hàng hiện có</h3>
-                <div class="card-tools">
-                  <div class="input-group input-group-sm">
-                    <input type="text" name="table_search" class="form-control float-right" placeholder="Tìm kiếm">
+        <?php include('widget/slider-bar.php') ?>
 
-                    <div class="input-group-append">
-                      <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Khách Hàng</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Trang Chủ</a></li>
+                                <li class="breadcrumb-item active">Khách hàng</li>
+                            </ol>
+                        </div>
                     </div>
-                    <div class="input-group-append">
-                      <a href="addnew-customer.php" class="btn btn-default"><i class="fas fa-plus-square text-warning"></i> Thêm mới khách hàng</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body table-responsive p-0" style="height: 600px;">
-                <table class="table table-head-fixed text-nowrap">
-                  <thead>
-                    <tr>
-                      <th><strong>STT</strong></th>
-                      <th><strong>Tên</strong></th>
-                      <th><strong>Số điện thoại</strong></th>
-                      <th><strong>Sinh nhật</strong></th>
-                      <th><strong>Ghi chú</strong></th>
-                      <th><strong>Gợi ý</strong></th>
-                      <th><strong>#</strong></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Nguyễn Thị Lé</td>
-                      <td>0906445584</td>
-                      <td><span class="tag tag-danger">11/12/1986</span></td>
-                      <td>Áo đen màu trắng.</td>
-                      <th>Còn hai ngày là sinh nhật</th>
-                      <th></th>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
-        </div>
-        </div>
-      </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+                </div><!-- /.container-fluid -->
+            </section>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-   <!-- Main Footer -->
-  <?php
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Danh sách khách hàng</h3>
+
+                                    <div class="card-tools">
+                                        <div class="input-group input-group-sm" style="width: 250px;">
+                                            <input type="text" name="table_search" class="form-control float-right"
+                                                placeholder="Tìm kiếm">
+
+                                            <div class="input-group-append">
+                                                <button type="submit" class="btn btn-default"><i
+                                                        class="fas fa-search"></i></button>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <a href="addnew-customer.php" class="btn btn-default"><i
+                                                        class="fas fa-plus-square text-warning"></i> Thêm</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body table-responsive p-0">
+                                    <table class="table table-head-fixed text-nowrap">
+                                        <thead>
+                                            <tr>
+                                                <th>STT</th>
+                                                <th>Tên Khách Hàng</th>
+                                                <th>Số điện thoại</th>
+                                                <th>Ngày Sinh</th>
+                                                <th>Ghi chú</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>183</td>
+                                                <td>John Doe</td>
+                                                <td>11-7-2014</td>
+                                                <td><span class="tag tag-success">Approved</span></td>
+                                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
+                                                    doner.</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5">
+                            <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Có tổng
+                                cộng 120 khách hàng</div>
+                        </div>
+                        <div class="col-sm-12 col-md-7">
+                            <div class="dataTables_paginate paging_simple_numbers">
+                                <ul class="pagination">
+                                    <li class="paginate_button page-item previous" id="example2_previous"><a href="#"
+                                            aria-controls="example2" data-dt-idx="0" tabindex="0" class="page-link">
+                                            <<< /a>
+                                    </li>
+                                    <li class="paginate_button page-item active"><a href="#" aria-controls="example2"
+                                            data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="example2"
+                                            data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="example2"
+                                            data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="example2"
+                                            data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="example2"
+                                            data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                                    <li class="paginate_button page-item"><a href="#" aria-controls="example2"
+                                            data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                                    <li class="paginate_button page-item next disabled" id="example2_next"><a href="#"
+                                            aria-controls="example2" data-dt-idx="7" tabindex="0"
+                                            class="page-link">>></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <?php
   include('widget/footer.php');
-  ?>
+ ?>
