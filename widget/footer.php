1bb3623320d6e14896437329cb683612b085b522
Tran Phuoc Hung <admin@tygon.com.vn>
2020-02-10 08:44:29 +0700

    141

--- widget/footer.php
+++ widget/footer.php
@@ -20,6 +20,5 @@
 <!-- OPTIONAL SCRIPTS -->
 <script src="plugins/chart.js/Chart.min.js"></script>
 <script src="dist/js/demo.js"></script>
-<script src="dist/js/pages/dashboard3.js"></script>
 </body>
 </html>
