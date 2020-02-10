785012bc8ef54d6c0e46334ac61e83630d47b3b2
Tran Phuoc Hung <admin@tygon.com.vn>
2020-02-10 08:23:21 +0700

    fix app

--- widget/header.php
+++ widget/header.php
@@ -41,6 +41,7 @@ endif;
   <link rel="stylesheet" href="dist/css/ionic.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
+
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
